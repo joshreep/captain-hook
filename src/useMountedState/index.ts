bf0816f1bd3c6af78885314ca34b2ab46b0315ef
Josh Reep <josh.reep@sstid.com>
2021-05-24 19:39:14 -0400

    adds useMountedState

---
+++ src/useMountedState/index.ts
@@ -0,0 +1,16 @@
+import { Dispatch, SetStateAction, useState } from 'react'
+import useIsMounted from '../useIsMounted'
+
+export default function useMountedState<T>(initialState: T): [T, Dispatch<SetStateAction<T>>] {
+    const [state, setState] = useState<T>(initialState)
+    const isMounted = useIsMounted()
+
+    function setStateIfMounted(newState: SetStateAction<T>) {
+        if (isMounted.current) {
+            if (newState instanceof Function) setState((previousState) => newState(previousState))
+            else setState(newState)
+        }
+    }
+
+    return [state, setStateIfMounted]
+}
---
+++ src/useMountedState/test.ts
@@ -0,0 +1,46 @@
+import { act, renderHook } from '@testing-library/react-hooks'
+import useMountedState from '.'
+
+function setup<T>(initialState: T) {
+    const utils = renderHook(() => useMountedState<T>(initialState))
+
+    return { ...utils }
+}
+
+describe('useMountedState', () => {
+    test('should set initialState', () => {
+        const { result } = setup('foo')
+        expect(result.current[0]).toBe('foo')
+    })
+
+    test('should setState if component is mounted', () => {
+        const { result } = setup('foo')
+
+        act(() => {
+            result.current[1]('bar')
+        })
+
+        expect(result.current[0]).toBe('bar')
+    })
+
+    test('should not setState if component is not mounted', () => {
+        const { result, unmount } = setup('foo')
+
+        unmount()
+        act(() => {
+            result.current[1]('bar')
+        })
+
+        expect(result.current[0]).toBe('foo')
+    })
+
+    test('should be able to pass in a dispatch function', () => {
+        const { result } = setup('foo')
+
+        act(() => {
+            result.current[1]((previous) => previous + 'bar')
+        })
+
+        expect(result.current[0]).toBe('foobar')
+    })
+})
